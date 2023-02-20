c72c14426938a587fca5141bcaa8b4e14dc60fca
Sumanth Talusani <125733051+tsrtravel@users.noreply.github.com>
2023-02-20 07:04:01 +0000

    Applied Stylesheets

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route("/")
+from flask import Flask, render_template
+# flask is a module
+# Flask is a class within the module flask
+#render_template is to render the html with templates folder
+
+app=Flask(__name__)  #creating an object
+@app.route("/")  # routing the main page from url to beyond ""/""
 def hello_world():
- return "Hello Sumanth"
+ return render_template('home.html')
 
 if __name__=="__main__":
   print("I am inside the If Loop")
+  #defining the local server to where the code needs to be run which is by default 0.0.0.0
   app.run(host='0.0.0.0',debug=True)
